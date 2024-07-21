218572f6202790fd2e0f6c3637617e2abf59b304
Andex7 <164794457+Andex7@users.noreply.github.com>
2024-07-21 20:08:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,12 +6,12 @@ function convertToBool(text, fault = 'true') {
 
 
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
-const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
+const BOTNAME = ANDEX MD™';
 const FOOTERNAME = '> ＡＮＤＥＸ -  ＭＤ - Ｖ1';
 global.owner = process.env.OWNER_NUMBER  || '94775107287' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=2dMRlADK#bgqHjbMAtloTZE2yVcWAwnZMP5Snp65ihVe0SINe_-U' ,
+SESSION_ID: process.env.SESSION_ID || 'ANDEX MD=2dMRlADK#bgqHjbMAtloTZE2yVcWAwnZMP5Snp65ihVe0SINe_-U' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
